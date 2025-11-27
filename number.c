d741d40350e0a5ebf7b4a83845948f5db9d23daf
tirumalamounika28-byte <tirumalamounika28@gmail.com>
2025-11-27 15:00:27 +0530

    Implement number comparison in number.c
    Fix formatting issues and add logic to compare two numbers.

---
+++ number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int a,b;
+printf("enter two numbers:");
+scanf("%d %d",&a,&b);
+if(a>b)
+  printf(%d is a large a");
+  else
+printf(%d is asmaller b");
+return 0;
